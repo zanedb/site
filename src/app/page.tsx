3684b8a3f0137c02e2a1eb970aafcd62ce599b66
Zane <github@zane.sh>
2024-07-13 19:47:59 -0700

    Query the age old API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Projects from '@/components/projects'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const age = await getAge()
+
   return (
-    <main className="p-12 max-w-screen-lg w-full mx-auto max-sm:p-8 max-sm:pt-10">
-      <h1 className="text-4xl max-sm:text-3xl mb-2">
     <main className="p-8 pt-0 md:p-12 max-w-screen-lg w-full mx-auto">
       <div className="grid gap-8 md:gap-16 md:grid-cols-2">
         <Image
@@ -15,6 +15,10 @@ export default function Home() {
         />
         <div className="">
           <h1 className="text-6xl max-sm:text-5xl font-bold">Hi, I’m Zane.</h1>
+          <p className="text-2xl max-sm:text-xl pt-4">
+            I’m a {age}-year-old web designer and musician, studying multimedia
+            arts.
+          </p>
         </div>
       </div>
       {/*
@@ -35,4 +39,16 @@ export default function Home() {
       <Projects />*/}
     </main>
   )
+}
+
+async function getAge() {
+  const res = await fetch('https://api.zanedb.com/api/age')
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  const json = await res.json()
+  return json.age
 }
