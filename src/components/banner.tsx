705c38f179d22634a53c824d88915ece10a72145
Zane <github@zane.sh>
2024-09-23 14:37:03 -0700

    Only show banner with /?banner flag

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,5 +1,12 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
 export default function Banner() {
-  return (
+  const searchParams = useSearchParams()
+  const enabled = searchParams.get('banner')
+
+  return enabled !== null ? (
     <marquee className="py-1 border-y-2 border-gray-800 bg-[#89CC04] uppercase text-sm tracking-widest">
       HIRE ME &middot; hire me &middot; hire me &middot; hire me &middot; hire
       me &middot; hire me &middot; hire me &middot; hire me &middot; hire me
@@ -8,5 +15,7 @@ export default function Banner() {
       &middot; hire me &middot; hire me &middot; hire me &middot; hire me
       &middot; hire me &middot; hire me &middot; hire me
     </marquee>
+  ) : (
+    <></>
   )
 }
