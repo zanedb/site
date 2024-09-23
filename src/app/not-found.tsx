288879dc1c8e92f34d501425183e9e3524b5e2f1
Zane <github@zane.sh>
2024-09-23 16:44:19 -0700

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <section className="leading-10 flex items-center">
+        <h1 className="mr-5 pr-6 text-2xl font-medium">404</h1>
+        <p>This page could not be found.</p>
+      </section>
+    </div>
+  )
+}
