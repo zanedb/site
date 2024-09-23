022f8099ef3034fe550960016a0d009871b0b2d5
Zane <github@zane.sh>
2024-09-23 14:36:54 -0700

    Increase tracking

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <marquee className="py-1 border-y-2 border-gray-800 bg-[#89CC04] uppercase text-sm ">
+    <marquee className="py-1 border-y-2 border-gray-800 bg-[#89CC04] uppercase text-sm tracking-widest">
       HIRE ME &middot; hire me &middot; hire me &middot; hire me &middot; hire
       me &middot; hire me &middot; hire me &middot; hire me &middot; hire me
       &middot; hire me &middot; hire me &middot; hire me &middot; hire me
