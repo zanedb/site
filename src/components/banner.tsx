73b5cdf3f91b5f7ffab1a55cfb57ca5d3bb8cc4e
Zane <github@zane.sh>
2024-09-26 16:45:24 -0700

    Wrap banner in suspense

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -3,24 +3,28 @@
 import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
-export default function Banner() {
+function PotentialBanner() {
   const searchParams = useSearchParams()
   const enabled = searchParams.get('banner')
 
+  return enabled !== null ? (
+    <div className="py-1 border-y-2 border-gray-800 text-black bg-[#89CC04] uppercase text-sm tracking-widest whitespace-nowrap overflow-hidden">
+      HIRE ME &middot; hire me &middot; hire me &middot; hire me &middot; hire
+      me &middot; hire me &middot; hire me &middot; hire me &middot; hire me
+      &middot; hire me &middot; hire me &middot; hire me &middot; hire me
+      &middot; hire me &middot; hire me &middot; hire me &middot; hire me
+      &middot; hire me &middot; hire me &middot; hire me &middot; hire me
+      &middot; hire me &middot; hire me &middot; hire me
+    </div>
+  ) : (
+    <></>
+  )
+}
+
+export default function Banner() {
   return (
     <Suspense>
-      {enabled !== null ? (
-        <div className="py-1 border-y-2 border-gray-800 text-black bg-[#89CC04] uppercase text-sm tracking-widest whitespace-nowrap overflow-hidden">
-          HIRE ME &middot; hire me &middot; hire me &middot; hire me &middot;
-          hire me &middot; hire me &middot; hire me &middot; hire me &middot;
-          hire me &middot; hire me &middot; hire me &middot; hire me &middot;
-          hire me &middot; hire me &middot; hire me &middot; hire me &middot;
-          hire me &middot; hire me &middot; hire me &middot; hire me &middot;
-          hire me &middot; hire me &middot; hire me &middot; hire me
-        </div>
-      ) : (
-        <></>
-      )}
+      <PotentialBanner />
     </Suspense>
   )
 }
